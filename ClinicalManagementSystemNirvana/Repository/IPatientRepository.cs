977e4aa2fd736f6b9fc87d8afdfd5868677ff581
Sameena Feroze <sameena.feroze@experionglobal.com>
2022-02-02 17:02:50 +0530

    a

---
+++ ClinicalManagementSystemNirvana/Repository/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using ClinicalManagementSystemNirvana.View_Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicalManagementSystemNirvana.Repository
+{
+    interface IPatientRepository
+    {
+        Task<List<PatientViewModel>> GetPatientAndPrescription();
+    }
+}
