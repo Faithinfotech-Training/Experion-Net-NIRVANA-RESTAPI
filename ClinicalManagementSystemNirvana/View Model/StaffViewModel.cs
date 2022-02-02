ee20513c3f3dba1d78747bffecd1fbce7806b827
Abhinav Jaiju <abhinav.jaiju@experionglobal.com>
2022-02-02 14:53:25 +0530

    Staff second commit by abhinav

--- ClinicalManagementSystemNirvana/View Model/StaffViewModel.cs
+++ ClinicalManagementSystemNirvana/View Model/StaffViewModel.cs
@@ -7,5 +7,14 @@ namespace ClinicalManagementSystemNirvana.View_Model
 {
     public class StaffViewModel
     {
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public string StaffIsActive { get; set; }
+        public string StaffAddr { get; set; }
+        public DateTime? StaffDob { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public int? RoleId { get; set; }
+        public string RoleName { get; set; }
     }
 }
