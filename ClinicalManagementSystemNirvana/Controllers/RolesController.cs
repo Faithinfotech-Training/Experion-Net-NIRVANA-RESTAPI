8f730edd714e538a11896396838f912936d4d7e1
Abhinav Jaiju <abhinav.jaiju@experionglobal.com>
2022-02-14 14:46:15 +0530

    Roles

---
+++ ClinicalManagementSystemNirvana/Controllers/RolesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinicalManagementSystemNirvana.Models;
+using ClinicalManagementSystemNirvana.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicalManagementSystemNirvana.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        //data fields
+        private readonly IRoleRepository _roleRepository;
+
+        //constructor injection
+        public RolesController(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+        #region Get All Department
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Roles>>> GetDepartmentALL()
+        {
+            return await _roleRepository.GetAllRoles();
+        }
+        #endregion
+    }
+}
---
+++ ClinicalManagementSystemNirvana/Repository/IRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClinicalManagementSystemNirvana.Models;
+
+namespace ClinicalManagementSystemNirvana.Repository
+{
+    public interface IRoleRepository
+    {
+        Task<List<Roles>> GetAllRoles();
+    }
+}
