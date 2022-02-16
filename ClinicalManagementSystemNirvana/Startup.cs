aae5a7554f5b134788715a0575628ea6ae889e3d
Sneha K Salu <sneha.salu@experionglobal.com>
2022-02-16 15:46:41 +0530

    commit by sneha

--- ClinicalManagementSystemNirvana/Startup.cs
+++ ClinicalManagementSystemNirvana/Startup.cs
@@ -46,6 +46,7 @@ namespace ClinicalManagementSystemNirvana
             services.AddScoped<IInventory, InventoryRepo>();
             services.AddScoped<IMedLabPresc, MedLabPrescRepo>();
             services.AddScoped<IStaffRepository, StaffRepository>();
+            services.AddScoped<ILabReportRepository, LabReportRepository>();
 
             services.AddControllers().AddNewtonsoftJson(
                 options =>
